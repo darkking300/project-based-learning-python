ab216f13f04ef2427eefb0a155adf915d6345a10
Asutosh Satapathy <asatapathy.78@gmail.com>
2020-11-28 16:06:12 +0530

    Removing Unnecessary functionalities
    Removed:
    1.subprocess module and code for moving images to folder
    2.folder creation using the data in title tag
    3.traceback module as the stacktrace not needed for general purpose standpoint

--- 4chan Image Downloader/main.py
+++ 4chan Image Downloader/main.py
@@ -1,24 +1,19 @@
 from urllib.request import Request, urlopen, URLopener  
 from html.parser import HTMLParser
 import os
-import traceback
 from sys import argv
-import subprocess
 
 baseUrl = argv[1]
 
 class LinkParse(HTMLParser):
 	linkStore = set()
 	folderName = ''
-	def handle_starttag(self,tag,attrs):
-		if tag == 'title' and not(os.path.exists('title')):
-			createFolder(attrs)
-			folderName = tag
-		elif tag == 'img':
-			for (param,value) in attrs:
-				if param == 'src':
-					self.linkStore.add(value)
-
+	def handle_starttag(self,tag,attrs):			
+	    if tag == 'img':
+		    for (param,value) in attrs:
+			    if param == 'src':
+				    self.linkStore.add(value)
+		
 	def __init__(self):
 		super().__init__()
 		
@@ -34,7 +29,7 @@ def urlEdit(imgUrl):
 #function to remove the small s in the file name which causes smaller size image to download
 	sliced = imgUrl.split('.')
 	sliced[-2] = sliced[-2][0:-1]
-	return '.'.join(sliced)
+	return '.'.join(sliced).strip()
 	
 	
 def downloadFile(linkStore):
@@ -43,14 +38,13 @@ def downloadFile(linkStore):
 			#removing double slash from the start of url
 			imgUrl = urlEdit(imgUrl[2:])
 			fileName = imgUrl.split("/")[-1]
+			imgUrl = 'https://' + imgUrl
 			print('Downloading file: '+fileName+'\tURL: '+imgUrl+'\n')
 			image = URLopener()
 			image.retrieve(imgUrl,fileName)
 			# above line may create error due to 403 forbidden response
 		except:
-			print("Error occured while downloading")
-			traceback.print_exc()	
-		print("\n")	
+			print("Error occured while downloading file: " + imgUrl +'\n')
 		continue
 
 def theCoreFunction():
@@ -61,10 +55,8 @@ def theCoreFunction():
 		f = LinkParse()
 		f.feed(str(response))
 		downloadFile(f.linkStore)
-		#subprocess.check_call('mv','*.jpg','*.png','*.webm','./'+f.folderName)
 	except:
-		print("Excpetion Occured\n")
-		traceback.print_exc()
+		print('An error occured while processing task.')
 		
 		
 theCoreFunction()		
