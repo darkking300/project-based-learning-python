ce1567a707f976a5849d29e83c9b060463c66aa7
Asutosh Satapathy <asatapathy.78@gmail.com>
2020-11-28 15:23:25 +0530

    Updated LinkFinder.py

--- Web Crawler/LinkFinder.py
+++ Web Crawler/LinkFinder.py
@@ -2,7 +2,15 @@ from html.parser import HTMLParser
 from urllib import parse
 
 class LinkFinder(HTMLParser):
+
+    def handle_starttag(self,tag,attrs):
+        if tag == 'href':
+            for (param,value) in attrs:
+                if param == 'href':
+                    print(value)
+
+
     def __init__(self):
-        super.__init__()
+        super().__init__()
     def error(self,message):    
         pass
